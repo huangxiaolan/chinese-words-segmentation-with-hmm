f071ad149feed551dc4712ca971dc03f338790a0
huangxiaolan <1257830250@qq.com>
2017-04-28 11:55:18 +0800

    init project

---
+++ data.py
@@ -0,0 +1,8 @@
+#coding=utf8
+
+data=[{
+ u"我要吃饭":"SSBE"},
+{
+u"天气不错" : "BEBE"},
+{
+u"谢天谢地" : "BMME"}]
---
+++ hmm_start.py
@@ -0,0 +1,32 @@
+#coding=utf8
+print(__doc__)
+
+import numpy as np
+#import matplotlib.pyplot as plt
+
+from hmmlearn import hmm
+import get_hmm_param as pa
+
+from hmmlearn.hmm import MultinomialHMM as mhmm
+startprob=np.array(pa.get_startprob())
+print("startprob is ",startprob)
+transmat=np.array(pa.get_transmat())
+print("transmat is ",transmat)
+emissionprob=np.array(pa.get_emissionprob())
+print("emmissionprob is ",emissionprob)
+mul_hmm=mhmm(n_components=4)
+
+mul_hmm.startprob_=startprob
+
+mul_hmm.transmat_=transmat
+
+mul_hmm.emissionprob_=emissionprob
+
+phase=u"我要吃饭谢天谢地"
+
+X=np.array(pa.get_array_from_phase(phase))
+X=X.reshape(len(phase),1)
+print("X is ",X)
+
+Y=mul_hmm.predict(X)
+print("Y is ",Y)
